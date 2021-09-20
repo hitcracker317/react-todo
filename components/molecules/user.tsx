c2876a293959306603b8d9b9ffe24ea1ef361285
hitcracker317 <dainaso97@gmail.com>
2021-09-20 23:34:04 +0900

    簡易的にサイドメニューのユーザーモジュール作成

---
+++ components/molecules/user.tsx
@@ -0,0 +1,12 @@
+export const User = () => {
+  return (
+    <div className="user-wrapper flex align-items--end">
+      <img className="user-avator" alt="xxxx" src="https://bit.ly/sage-adebayo" />
+      <div className="user-text-wrapper ">
+        <h3>Andrew dressy</h3>
+        <p>dynamaxcity@gmail.com</p>
+      </div>
+    </div>
+  )
+}
+
--- components/organisms/sidemenu.tsx
+++ components/organisms/sidemenu.tsx
@@ -1,6 +1,9 @@
+import { User } from 'components/molecules/user'
+
 export const SideMenu = () => {
   return (
     <aside>
+      <User />
       サイドメニューです
     </aside>
   )
